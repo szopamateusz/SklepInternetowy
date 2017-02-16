b3e6a90d433add4c1022cf95e0fd0ae5beec4e0d
Mateusz <kontakt@mateusz-szopa.pl>
2017-02-16 12:07:13 +0100

    Dodanie atrybutów do klas naszego modelu

--- PraktyczneKursy/PraktyczneKursy/Models/Kategoria.cs
+++ PraktyczneKursy/PraktyczneKursy/Models/Kategoria.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PraktyczneKursy.Models
 {
     public class Kategoria
     {
         public int KategoriaId { get; set; }
+        [Required(ErrorMessage = "Wprowadź nazwę kategorii")]
+        [StringLength(100)]
         public string NazwaKategori { get; set; }
+        [Required(ErrorMessage = "Wprowadź opis kategorii")]
         public string OpisKategori { get; set; }
         public string NazwaPlikuIkony { get; set; }
 
--- PraktyczneKursy/PraktyczneKursy/Models/Zamowienie.cs
+++ PraktyczneKursy/PraktyczneKursy/Models/Zamowienie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,21 @@ namespace PraktyczneKursy.Models
     public class Zamowienie
     {
         public int ZamowienieId { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Imię")]
+        [StringLength(50)]
         public string Imie { get; set; }
+        [Required(ErrorMessage = "Wprowadź Nazwisko")]
+        [StringLength(50)]
         public string Nazwisko { get; set; }
+        [Required(ErrorMessage = "Wprowadź Ulicę")]
+        [StringLength(100)]
         public string Ulica { get; set; }
+        [Required(ErrorMessage = "Wprowadź Miasto")]
+        [StringLength(100)]
         public string Miasto { get; set; }
+        [Required(ErrorMessage = "Wprowadź Kod Pocztowy")]
+        [StringLength(6)]
         public string KodPocztowy { get; set; }
         public string Telefon { get; set; }
         public string Email { get; set; }
