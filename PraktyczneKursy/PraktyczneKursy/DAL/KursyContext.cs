2af93fabf5bc5515541064db9e3c1063ceab7f61
Mateusz <kontakt@mateusz-szopa.pl>
2017-02-16 12:22:11 +0100

    Dodanie KursyContext

---
+++ PraktyczneKursy/PraktyczneKursy/DAL/KursyContext.cs
@@ -0,0 +1,28 @@
+﻿using PraktyczneKursy.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace PraktyczneKursy.DAL
+{
+    public class KursyContext : DbContext
+    {
+        public KursyContext() : base("KursyContext")
+        {
+                
+        }
+        public DbSet<Kurs> Kursy { get; set; }
+        public DbSet<Kategoria> Kategorie { get; set; }
+        public DbSet<Zamowienie> Zamowienia { get; set; }
+        public DbSet<PozycjeZamowienia> PozycjeZamowienia { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
