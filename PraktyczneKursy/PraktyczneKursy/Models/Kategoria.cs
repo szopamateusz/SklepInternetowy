ac6c3c564874ac60ec0bf6540e32b0926a87a183
Mateusz <kontakt@mateusz-szopa.pl>
2017-02-16 11:58:41 +0100

    Dodanie klas modelu dla naszej aplikacji

---
+++ PraktyczneKursy/PraktyczneKursy/Models/Kategoria.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PraktyczneKursy.Models
+{
+    public class Kategoria
+    {
+        public int KategoriaId { get; set; }
+        public string NazwaKategori { get; set; }
+        public string OpisKategori { get; set; }
+        public string NazwaPlikuIkony { get; set; }
+
+        public virtual ICollection<Kurs> Kursy { get; set; }
+    }
+}
---
+++ PraktyczneKursy/PraktyczneKursy/Models/PozycjeZamowienia.cs
@@ -0,0 +1,13 @@
+﻿namespace PraktyczneKursy.Models
+{
+    public class PozycjeZamowienia
+    {
+        public int PozycjaZamowieniaId { get; set; }
+        public int ZamowienieId { get; set; }
+        public int KursId { get; set; }
+        public decimal CenaZakupu { get; set; }
+
+        public virtual Kurs kurs { get; set; }
+        public virtual Zamowienie zamowienie { get; set;}
+    }
+}
